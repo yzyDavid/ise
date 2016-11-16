b9cdc82d2e873650e7d89f14bbb9b590d0619ff1
yzyDavid <yzyDavid@qq.com>
2016-11-16 17:58:51 +0000

    finished verilog part in proj2

---
+++ Hex427Seg/clkdiv.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:50:59 11/16/2016 
+// Design Name: 
+// Module Name:    clkdiv 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clkdiv(input clk,
+					input rst,
+					output reg[31:0] clkdiv
+    );
+	 always @ (posedge clk or posedge rst) begin
+		if (rst) clkdiv <= 0;
+		else clkdiv <= clidiv + 1'b1;
+	end
+
+endmodule
